fd176053d39fec70d0018f574d28d784b342fd29
Silvio Antonio <oliveira_0000@hotmail.com>
2019-09-14 09:27:35 -0300

    Configurando PostgreSQL

--- src/main/java/com/silvio/eventospring/DataConfiguration.java
+++ src/main/java/com/silvio/eventospring/DataConfiguration.java
@@ -1,6 +1,9 @@
 package com.silvio.eventospring;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,10 +18,12 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
     
+    
+    //CONFIGURAÇAO DA CONECÇAO DO MYSQL E JPA
     /**
      * Bean de configuraçao de coneccao com o banco
      * @return coneccao com o banco
-     */
+     *
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -38,6 +43,23 @@ public class DataConfiguration {
         adapter.setDatabasePlatform("org.hibernate.dialect.MySQL57Dialect");
         adapter.setPrepareConnection(true);
         return adapter;
+    }*/
+    
+    //CONNECTION CONFIGURATION OF HEROKU POSTGRESQL
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
     
 }
